f54fcdd1d9bf91ff83d8e9a9b858b072b0e096d0
Xeltica <xeltica@gmail.com>
2021-05-26 02:35:47 +0900

    静的ファイルのコピーができない問題を修正

--- lib/build/static.ts
+++ lib/build/static.ts
@@ -5,11 +5,12 @@ import logIf from '../log-if';
 
 export default async (path: string, silent = false) => new Promise<void>(res => {
     const dest = path.replace('src', 'dist');
-    const dir = _path.dirname(dest) + dest.slice('dist/assets'.length);
+    const dir = _path.dirname(dest);
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
     }
-    fs.copyFile(path, dir, () => {
+    console.log(dir);
+    fs.copyFile(path, dest, () => {
         logIf(!silent, 'Copied ' + path);
         res();
     });
